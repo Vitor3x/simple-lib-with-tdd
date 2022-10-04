1de57fa0211c5f99f257af47b863c1cdc1b412b9
Joao Vitor <viictorsouza97@gmail.com>
2022-10-03 21:17:16 -0300

    refactor: remove returns on function queryString

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,7 +1,4 @@
-export function queryString(obj) {
-  const entries = Object.entries(obj).map(item => {
-    return `${item[0]}=${item[1]}`;
-  });
-
-  return entries.join('&');
-}
+export const queryString = obj =>
+  Object.entries(obj)
+    .map(([key, value]) => `${key}=${value}`)
+    .join('&');
