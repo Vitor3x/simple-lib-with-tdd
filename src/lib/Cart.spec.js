8f8516ccd13dc1f01b30e1feb682d487b77a35a6
Joao Vitor <viictorsouza97@gmail.com>
2022-10-04 23:11:03 -0300

    test: calculate total cart

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -1,10 +1,29 @@
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 import Cart from './Cart';
 
 describe('Cart', () => {
+  let cart;
+  beforeEach(() => {
+    cart = new Cart();
+  });
+
   it('should return 0 when getTotal() is executed in a newly created instance', () => {
     const cart = new Cart();
 
     expect(cart.getTotal()).toEqual(0);
+  });
+
+  it('should multiply quantity and price and receive the total amount', () => {
+    const item = {
+      quantity: 2,
+      product: {
+        title: 'Headset',
+        price: 2000,
+      },
+    };
+
+    cart.add(item);
+
+    expect(cart.getTotal()).toEqual(4000);
   });
 });
